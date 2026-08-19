c140af95928906f1b2c8517ee6b33c3aec8b5553
LucasDantas10 <lucas.dantas1078@gmail.com>
2026-08-19 17:06:42 -0300

    Update: Melhoria de código

--- PHPVanilla/DesafioSala/desafioSimulador.php
+++ PHPVanilla/DesafioSala/desafioSimulador.php
@@ -1,10 +1,10 @@
 <?php
-
-$categoriaCliente = "A";
+echo "Digite a Categoria do Cliente";
+$categoriaCliente = readline();
 $saldoDevedor = 1000.00;
 $mesesAtraso = 12;
 
-//Definir a Taxa de Juros de Acordo com a Classsificação do Cliente
+//RF01 -> Definir a Taxa de Juros de Acordo com a Classsificação do Cliente
 $txJuros = match ($categoriaCliente) {
     "A" => 0.01 ,
     "B" => 0.02 ,
@@ -13,10 +13,10 @@ $txJuros = match ($categoriaCliente) {
 };
 
 //Demonstrar a Evolução da Divida ao Longo de 12 meses
-
+//RF02 -> 
 for ($i=1; $i <= $mesesAtraso; $i++) { 
 
-    //Regra Especial : Anistia no mês do juros 6
+    //Regra Especial : Anistia no mês do juros 6 => RF-03
     if($i===6){
         echo "\nValor ao final do mês $i " . number_format($saldoDevedor,2,",",".");
         continue;
